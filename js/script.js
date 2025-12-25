d10eb328042b0e4f1555e51ee0df6103533e3cca
sai <vanamakrishnagurusaivkgs@gmail.com>
2025-12-25 16:34:24 +0530

    Added Javascript boilerplate and DOM references

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById("transaction-form");
+const text = document.getElementById("text");
+const amount = document.getElementById("amount");
+const list = document.getElementById("list");
+const balance = document.getElementById("balance");
+
+let transactions = [];
